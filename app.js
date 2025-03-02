288710b9cfd598bcde77d8942779d3d8a50d3715
TsotneIvanashvili <Tsotneivanashvili@gmail.com>
2025-03-02 08:57:02 -0800

    main

--- app.js
+++ app.js
@@ -3,25 +3,36 @@ let section = document.querySelector(".room-container");
 fetch("https://hotelbooking.stepprojects.ge/api/Rooms/GetAll")
   .then(response => response.json())
   .then(data => {
-    data.forEach(item => {
+    // Limit the data to the first 9 items
+    const limitedData = data.slice(0, 9);
+    
+    limitedData.forEach(item => {
         section.innerHTML += cardCode(item);
     });
   })
   .catch(() => console.error("Connection error"));
 
-function cardCode(item) {
-    return `<div class="card" style="width: 18rem;">
-  <img src="${item.images[0]?.source}" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h4 class="card-title">${item.name}</h4>
-    <p>Price per night: $${item.pricePerNight}</p>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a onclick="gotoDetails('${item.id}')" class="btn btn-primary">View Details</a>
-  </div>
-</div>`;
+  function cardCode(item) {
+    return `<div class="room-card">
+        <img class="cardImg" src="${item.images[0]?.source}" alt="">
+        <div class="main-txt">
+            <div class="txt">
+                <h3>${item.name}</h3>
+            </div>
+            <div class="price"> 
+                <p>${item.pricePerNight}$</p>
+                <p>a night</p>
+            </div>
+        </div>
+        <div class="hover-content">
+                <h3>${item.address}</h3>
+                <h3></h3>
+                <h3></h3>
+                <p>${item.pricePerNight}$</p>
+            <a onclick="gotoDetails('${item.id}')" class="button">Book Now</a>
+        </div>
+    </div>`;
 }
-
-
 
 function gotoDetails(roomId) {
     window.location.href = `details.html?id=${roomId}`;
