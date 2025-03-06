975b783c21960664c42c5cb387661f58aa7f20d3
Tsotnevanashvili <Tsotneivanashvili@gmail.com>
2025-03-06 21:13:57 +0400

    main

---
+++ hotels.js
@@ -0,0 +1,18 @@
+let section = document.querySelector(".hotel-container");
+
+
+fetch("https://hotelbooking.stepprojects.ge/api/Hotels/GetAll")
+.then(response => response.json())
+.then( data => data.forEach(item => {
+    section.innerHTML += hotelCode(item);
+}))
+.catch(() => console.error("Connection error"));
+
+
+function hotelCode(item){
+    return `<div class="hotels">
+                <img class = "hotel-img" src="${item.featuredImage}" alt="">
+                <h1>${item.name}</h1>
+                <button id = "hotel-button">View Rooms</button>
+            </div>`
+}
